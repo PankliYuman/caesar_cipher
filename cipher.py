b9529908967c5bb5b3c830b976205f9ccbb05f6e
Porfirev <daniil.porfirev@sulzer.de>
2024-04-10 17:49:24 +0200

    When the position to which a letter should be shifted is out of bounds, the program will adjust it to the correct letter within the allowed range.

--- cipher.py
+++ cipher.py
@@ -2,24 +2,31 @@ eng_lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
 eng_upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 print("Welcome to Caesar Cipher Program")
-text = input("Enter your text")
+text = input("Enter your text: ")
 new_text = ""
+
+
+def shift_char(char, shift, alphabet):
+    if char in alphabet:
+        new_index = (alphabet.index(char) + shift) % len(alphabet)
+        return alphabet[new_index]
+    return char
+
+
 while True:
     try:
         number_of_position = int(input("Specify how many positions each letter should shift to the right (e.g., 5) or"
-                                       " to the left (e.g., -2) in the Caesar cipher."))
+                                       " to the left (e.g., -2) in the Caesar cipher: "))
         break
     except ValueError:
-        print("Please enter a number")
+        print("Please enter a valid number.")
 
-for i in range(len(text)):
-    if text[i] in eng_lower_alphabet:
-        new_text += eng_lower_alphabet[eng_lower_alphabet.find(text[i]) + number_of_position]
-        continue
-    elif text[i] in eng_upper_alphabet:
-        new_text += eng_upper_alphabet[eng_upper_alphabet.find(text[i]) + number_of_position]
-        continue
+for char in text:
+    if char.islower():
+        new_text += shift_char(char, number_of_position, eng_lower_alphabet)
+    elif char.isupper():
+        new_text += shift_char(char, number_of_position, eng_upper_alphabet)
     else:
-        new_text += text[i]
+        new_text += char
 
-print(new_text)
+print("Ciphered text:", new_text)
